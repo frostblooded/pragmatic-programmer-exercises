79d1f5a33dc5047e781b3f618e72bc075d7855b6
frostblooded <frostblooded@yahoo.com>
2017-06-06 10:09:20 +0300

    Complete exercise 6

--- 6_time_parser_normal/time_parser_normal.py
+++ 6_time_parser_normal/time_parser_normal.py
@@ -1,17 +1,45 @@
-import sys
-
 import re
+
+
+def print_res(hour, minute):
+    res = str(hour)
+
+    if minute_str:
+        res += ':' + minute_str
+    else:
+        res += ':00'
+
+    print(res)
+
 
 if __name__ == '__main__':
     hour = None
     minute = None
 
+    input_times = [
+        '4pm',
+        '7:38pm',
+        '23:42',
+        '3:16',
+        '3:16am'
+    ]
+
     parser_regex = r'([0-9]+)[:]?([0-9]+)?([AaPp][mM])?'
-    # input_time = sys.argv[1]
-    input_time = '4:36'
-    matches = re.match(parser_regex, input_time)
 
-    print(matches.groups())
+    for time in input_times:
+        matches = re.match(parser_regex, time)
 
-    print('Hour ' + str(hour))
-    print('Minute ' + str(minute))
+        hour_str = matches.group(1)
+        minute_str = matches.group(2)
+        am_pm = matches.group(3)
+        hour = int(hour_str)
+
+        if minute_str:
+            minute = int(minute_str)
+
+        am_pm_regex = r'([AaPp][mM])?'
+
+        if am_pm and re.fullmatch(am_pm_regex, am_pm):
+            hour += 12
+
+        print_res(hour, minute)
